c74b2f86e8ab14066a8b317489547b94308a2ec7
LieOnLion <lieonlion4@gmail.com>
2025-07-05 22:11:30 +0100

    removed cloudflare

--- app.config.ts
+++ app.config.ts
@@ -1,17 +1,8 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss(), cloudflare()],
-  },
-
-  server: {
-    preset: "cloudflare-pages",
-
-    rollupConfig: {
-      external: ["node:async_hooks"],
-    },
+    plugins: [tailwindcss()],
   },
 });
