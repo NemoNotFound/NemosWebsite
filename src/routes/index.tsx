a3fde0b66a80699d81717333b7b10d3ba4a8b6b1
NemoNotFound <02grimes-bard@icloud.com>
2025-07-06 17:04:18 +0200

    Fix downlaod count not being displayed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,17 +2,17 @@ import { Component, createEffect, createSignal } from "solid-js";
 import { CFMod } from "./api/curseforge/studios/v1/mod/[id]";
 
 const Home: Component = () => {
-  const [downloads, setDownloads] = createSignal<number>(0);
+  const [downloads, setDownloads] = createSignal<string>("0");
   const modId: number = 1148320;
 
   createEffect(async () => {
-    const mod: CFMod = await fetch(
+    const downloads: string = await fetch(
       `/api/curseforge/studios/v1/mod/${modId}/downloads`
     ).then((res) => {
       if (res.ok) return res.json();
     });
 
-    setDownloads(mod.downloads);
+    setDownloads(downloads);
   });
 
   return (
