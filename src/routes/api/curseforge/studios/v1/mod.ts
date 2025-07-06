4a6260edfffbc4c56156a959b143f90168e490bf
NemoNotFound <02grimes-bard@icloud.com>
2025-07-06 14:37:22 +0200

    Test

--- src/routes/api/curseforge/studios/v1/mod.ts
+++ src/routes/api/curseforge/studios/v1/mod.ts
@@ -13,5 +13,28 @@ export type CFMod = {
 };
 
 export const GET = async () => {
-  return {};
+    const modId = 1;
+    const apiKey = process.env.CF_API_KEY;
+
+    try {
+        const response = await fetch('https://api.curseforge.com/v1/mods/${modId}', {
+            method: 'GET'
+            headers: {
+                'x-api-key', apiKey,
+                'Accept': 'application/json'
+                }
+            });
+
+        const json = await respone.json();
+
+        return new Response(JSON.stringify(json.data.downloadCount), {
+            status: 200,
+             headers: {
+                    'Content-Type': 'application/json'
+                  }
+            });
+        } catch (e) {
+
+            return new Response('Internal Server Error', { status: 500 });
+            }
 };
