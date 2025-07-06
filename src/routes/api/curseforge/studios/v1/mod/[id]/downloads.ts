6fdc9c8aaf6afc0171dcc0162b2922f1f0f4155d
LieOnLion <lieonlion4@gmail.com>
2025-07-06 15:14:59 +0100

    maybe?

--- src/routes/api/curseforge/studios/v1/mod/[id]/downloads.ts
+++ src/routes/api/curseforge/studios/v1/mod/[id]/downloads.ts
@@ -1,13 +1,11 @@
 import type { APIEvent } from "@solidjs/start/server";
-import { CFMod } from ".";
+import { GET as IndexGET, CFMod } from ".";
 
 export const GET = async ({ params }: APIEvent) => {
   const modId = Number.parseInt(params.id);
-  const mod: CFMod = await fetch(`api/curseforge/studios/v1/mod/${modId}`).then(
-    (res) => {
-      if (res.ok) return res.json();
-    }
-  );
+  const mod: CFMod = await IndexGET({ params } as APIEvent).then((res) => {
+    if (res.ok) return res.json();
+  });
 
   return mod.downloads.toLocaleString("en-US", {
     notation: "compact",
