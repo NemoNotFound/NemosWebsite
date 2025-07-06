2cbd86173a0501c724abf767235364376c483583
LieOnLion <lieonlion4@gmail.com>
2025-07-06 14:18:43 +0100

    download count

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,30 @@
-import { Component } from "solid-js";
+import { Component, createEffect, createSignal } from "solid-js";
+import { CFMod } from "./api/curseforge/studios/v1/mod/[id]";
 
 const Home: Component = () => {
+  const [downloads, setDownloads] = createSignal<number>(0);
+  const modId: number = 1148320;
+
+  createEffect(async () => {
+    const mod: CFMod = await fetch(
+      `/api/curseforge/studios/v1/mod/${modId}`
+    ).then((res) => {
+      if (res.ok) return res.json();
+    });
+
+    setDownloads(mod.downloads);
+  });
+
   return (
     <main>
-      <div>hello world</div>
+      <div>
+        {downloads().toLocaleString("en-US", {
+          notation: "compact",
+          compactDisplay: "short",
+          minimumFractionDigits: 1,
+          maximumFractionDigits: 1,
+        })}
+      </div>
     </main>
   );
 };
